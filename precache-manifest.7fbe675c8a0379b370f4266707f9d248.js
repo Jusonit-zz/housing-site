784f8aea9d54f1eda164ebb56e886a7e001a5cda
Junior Figuereo <juniorsj@live.com>
2020-10-13 04:32:31 -0400

    Updates

---
+++ precache-manifest.7fbe675c8a0379b370f4266707f9d248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7853f55f2d60c282739ebb4b91f269",
+    "url": "/housing-site/index.html"
+  },
+  {
+    "revision": "d87b9138824786c582a9",
+    "url": "/housing-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f3734b90a2aaae333e8",
+    "url": "/housing-site/static/js/2.d1599947.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/housing-site/static/js/2.d1599947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87b9138824786c582a9",
+    "url": "/housing-site/static/js/main.7de84228.chunk.js"
+  },
+  {
+    "revision": "d534bc815d467f5929a7",
+    "url": "/housing-site/static/js/runtime-main.bb71b5a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/housing-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
